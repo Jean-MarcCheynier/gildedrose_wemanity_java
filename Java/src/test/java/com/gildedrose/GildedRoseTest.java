cb1da3719a5996cdac2c6da98f9b9e3d39b81309
Jean-Marc <jmcheynier@gmail.com>
2019-03-26 20:18:38 +0100

    UNIT TESTS AND TEXTTEST
    Improving test coverage and comparing result between old and new update method of the GildedRose class

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -4,14 +4,85 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.gildedrose.items.AgedBrie;
+import com.gildedrose.items.BackstagePass;
+import com.gildedrose.items.LegendaryItem;
+import com.gildedrose.items.StandardItem;
+import com.gildedrose.items.interfaces.UpdatableItem;
+
 public class GildedRoseTest {
 
     @Test
     public void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
-        app.updateQuality();
+        app.updateQualityV0();
         assertEquals("foo", app.items[0].name);
     }
+    
+    @Test
+    public void stadardUpdate() {
+        UpdatableItem[] items = new UpdatableItem[] { new  StandardItem("Standard", 1, 30) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(29, app.updatableItems[0].getQuality());
+        app.updateQuality();
+        assertEquals(27, app.updatableItems[0].getQuality());
+    }
+    
+    @Test
+    public void agedBrie() {
+        UpdatableItem[] items = new UpdatableItem[] { new  AgedBrie("Aged Brie", 1, 45) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(46, app.updatableItems[0].getQuality());
+        app.updateQuality();
+        assertEquals(48, app.updatableItems[0].getQuality());
+    }
+    
+    @Test
+    public void backstagePass() {
+        UpdatableItem[] items = new UpdatableItem[] { new  BackstagePass("Backstage passes to a TAFKAL80ETC concert", 2, 45) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(48, app.updatableItems[0].getQuality());
+        app.updateQuality();
+        assertEquals(50, app.updatableItems[0].getQuality());
+        app.updateQuality();
+        assertEquals(0, app.updatableItems[0].getQuality());
+    }
+    
+    @SuppressWarnings("deprecation")
+	@Test
+    public void oldVSNew() {
+        UpdatableItem[] items = new UpdatableItem[] { new  BackstagePass("Backstage passes to a TAFKAL80ETC concert", 2, 45),  new  AgedBrie("Aged Brie", 1, 45) };
+        Item[] oldItems = new Item[] { new  Item("Backstage passes to a TAFKAL80ETC concert", 2, 45),  new  Item("Aged Brie", 1, 45) };
+        GildedRose app = new GildedRose(items);
+        GildedRose oldApp = new GildedRose(oldItems);
+        app.updateQuality();
+        oldApp.updateQualityV0();
+        assertEquals(oldApp.items[0].quality, app.updatableItems[0].getQuality());
+        assertEquals(oldApp.items[1].quality, app.updatableItems[1].getQuality());
+        app.updateQuality();
+        oldApp.updateQualityV0();
+        assertEquals(oldApp.items[0].quality, app.updatableItems[0].getQuality());
+        assertEquals(oldApp.items[1].quality, app.updatableItems[1].getQuality());        
+        app.updateQuality();
+        oldApp.updateQualityV0();
+        assertEquals(oldApp.items[0].quality, app.updatableItems[0].getQuality());
+        assertEquals(oldApp.items[1].quality, app.updatableItems[1].getQuality());
+    }
+    
+    @Test
+    public void legendaryItem() {
+        UpdatableItem[] items = new UpdatableItem[] { new  LegendaryItem("Legendary", 2, 45) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(45, app.updatableItems[0].getQuality());
+        assertEquals(2, app.updatableItems[0].getSellIn());
+    }
+    
+    
+    
 
 }
--- Java/src/test/java/com/gildedrose/TexttestFixture.java
+++ Java/src/test/java/com/gildedrose/TexttestFixture.java
@@ -1,20 +1,29 @@
 package com.gildedrose;
 
+import com.gildedrose.items.AgedBrie;
+import com.gildedrose.items.BackstagePass;
+import com.gildedrose.items.LegendaryItem;
+import com.gildedrose.items.StandardItem;
+import com.gildedrose.items.Sulfuras;
+import com.gildedrose.items.interfaces.UpdatableItem;
+
 public class TexttestFixture {
     public static void main(String[] args) {
         System.out.println("OMGHAI!");
 
-        Item[] items = new Item[] {
-                new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+        UpdatableItem[] items = new UpdatableItem[] {
+                new StandardItem("+5 Dexterity Vest", 10, 20), //
+                new AgedBrie("Aged Brie", 2, 0), //
+                new StandardItem("Elixir of the Mongoose", 5, 7), //
+                new Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Sulfuras("Sulfuras, Hand of Ragnaros", -1, 80),
+                new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 10, 20),
+                new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 9, 20),
+                new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 5, 40),
+                new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 4, 40),
+                new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 4, 49),
                 // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+                new StandardItem("Conjured Mana Cake", 3, 6) };
 
         GildedRose app = new GildedRose(items);
 
@@ -26,12 +35,11 @@ public class TexttestFixture {
         for (int i = 0; i < days; i++) {
             System.out.println("-------- day " + i + " --------");
             System.out.println("name, sellIn, quality");
-            for (Item item : items) {
+            for (UpdatableItem item : items) {
                 System.out.println(item);
             }
             System.out.println();
             app.updateQuality();
         }
     }
-
 }
